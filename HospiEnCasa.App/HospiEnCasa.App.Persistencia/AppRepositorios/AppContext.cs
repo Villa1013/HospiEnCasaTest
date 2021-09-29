cc1be9454c7180f913071fb0bb015a55878f5a3a
villa1013 <williamvillalba13@gmail.com>
2021-09-28 22:11:04 -0500

    initial commit

---
+++ HospiEnCasa.App/HospiEnCasa.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+//using System;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Persistencia
+{
+    public class AppContext:DbContext
+    {
+        public DbSet<Persona> Personas {get;set;} //Al establecer la conexión asigna una tabla a la clase Persona, le doy el nombre Personas/
+        
+        //public DbSet<Medico> Medicos{get;set;}
+        //public DbSet<FamiliarDesignado> familiarDesignado{get;set;}
+        //public DbSet<Historia> Historia{get;set;}
+        //public DbSet<Paciente> Paciente{get;set;}
+        //public DbSet<SugerenciaCuidados> SugerenciaCuidados{get;set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //Variable del ef
+        {
+            if (!optionsBuilder.IsConfigured) //Valida si la configuración o conexión se realizó
+            {
+                optionsBuilder.UseSqlServer("Data source=(localdb)\\MSSQLLocalDB; Initial Catalog=HospiEnCasaData"); //Se define el servidor de la DB
+            }
+        }
+    }
+}
