5d15785dbcfd5abc8a4569407ce8b7a076b2ef5b
villa1013 <williamvillalba13@gmail.com>
2021-09-29 00:12:29 -0500

    Console Paciente

--- HospiEnCasa.App/HospiEnCasa.App.Consola/Program.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Consola/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
 
 namespace HospiEnCasa.App.Consola
 {
     class Program
     {
+        private static IRepositorioPaciente _repoPaciente= new RepositorioPaciente(new Persistencia.AppContext());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World EF!");
+            AddPaciente();
         }
+
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Nicolay",
+                Apellidos = "Perez",
+                NumeroTelefono = "3001645",
+                Genero = Genero.Masculino,
+                Direccion = "calle 4 No 7-4",
+                Longitud = 5.07062F,
+                Latitud = -75.52290F,
+                Ciudad = "Caldas",
+                FechaNacimiento = new DateTime(1990, 04, 12)
+            };
+            _repoPaciente.AddPaciente(paciente);
+        } 
     }
 }
